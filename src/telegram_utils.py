85b684fc82cbbf618d382b1f8574c445c2ec513a
Daniil Gusev <danilgusev9639@gmail.com>
2025-03-11 22:22:25 +0300

    clean the code

--- src/telegram_utils.py
+++ src/telegram_utils.py
@@ -1,35 +1,36 @@
-from telegram import Bot
 import asyncio
-from telegram import Bot
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from telegram import Bot
 
 load_dotenv()
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "YOUR_TELEGRAM_CHAT_ID")
 
 
-
-def send_telegram_notification(image_path, video_path):
+def send_telegram_notification(image_path, video_path, caption_dict={}):
     async def _send_notification():
         bot = Bot(token=TELEGRAM_BOT_TOKEN)
         # Send snapshot image
         try:
-            with open(image_path, 'rb') as img_file:
-                await bot.send_photo(chat_id=TELEGRAM_CHAT_ID,
-                                     photo=img_file,
-                                     caption="Event Snapshot")
+            with open(image_path, "rb") as image_file:
+                await bot.send_photo(
+                    chat_id=TELEGRAM_CHAT_ID, photo=image_file, caption=caption_dict.get("image_caption", "")
+                )
             print(f"Sent snapshot '{image_path}' successfully.")
         except Exception as e:
             print("Error sending snapshot:", e)
         # Send video clip
         try:
-            with open(video_path, 'rb') as video_file:
-                await bot.send_video(chat_id=TELEGRAM_CHAT_ID,
-                                     video=video_file,
-                                     caption="Event Video Clip")
+            with open(video_path, "rb") as video_file:
+                await bot.send_video(
+                    chat_id=TELEGRAM_CHAT_ID,
+                    video=video_file,
+                    caption=caption_dict.get("video_caption", ""),
+                )
             print(f"Sent video '{video_path}' successfully.")
         except Exception as e:
             print("Error sending video:", e)
+
     asyncio.run(_send_notification())
